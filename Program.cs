83d04f0a02f5c9a3e66fcf5ca6b69c54fac7a35c
Jayden Ranger <10012125@bcs.net.nz>
2017-08-03 15:41:45 +1200

    wk4-tut2

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace tutorial2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Give me a number");
+            var input1 = Console.ReadLine();
+            var number1 = 0;
+            var isNumber = int.TryParse(input1, out number1);
+            var Count = 1;
+           do{
+
+           
+            Console.WriteLine(number1 + " Times " + Count + " = "+ number1*Count);
+            Count++;
+            } while(Count < 13);
+        }
+    }
+}
